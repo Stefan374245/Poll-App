0e094ff688d99ba1c53a00913d3f87dadcaf486a
Stefan Helldobler <shelldobler@gmx.de>
2026-02-28 13:28:58 +0100

    feat: add barrel exports for shared components

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,14 @@
+// Barrel export for shared components
+export * from './btn/btn';
+export * from './category-tag/category-tag';
+export * from './countdown-badge/countdown-badge';
+export * from './input/input';
+export * from './question-block/question-block';
+export * from './result-bar/result-bar';
+export * from './result-block/result-block';
+export * from './results-header/results-header';
+export * from './spinner/spinner';
+export * from './status-badge/status-badge';
+export * from './survey-header/survey-header';
+export * from './survey-meta/survey-meta';
+export * from './toast/toast';
